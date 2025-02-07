eabae5438dc735841c6f2ec9bd5f73fe2c68365e
Dominic Leon Hackett <dominichackett@gmail.com>
2025-02-07 00:23:11 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,9 +18,9 @@ const Home: NextPage = () => {
         <Header />
         <main className="mt-20 grid lg:grid-cols-2 place-items-center pt-16 pb-8 md:pt-12 md:pb-24" >
        <div> <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold lg:tracking-tight xl:tracking-tighter" >
-Work Mate. <br/>Your AI Upwork Job  <br/> Search Assistant
+Work Mate. <br/>Your AI Freelancer Job  <br/> Search Assistant
 </h1><p className="text-lg mt-4 text-slate-600 max-w-xl" >
-an AI-powered agent designed to automate the process of finding freelance jobs on Upwork. It fetches job listings based on your search parameters, filters them using criteria like keywords, budget, and client history, and drafts personalized proposals for you to respond to.</p>
+an AI-powered agent designed to automate the process of finding freelance jobs on Freelancer.com. It fetches job listings based on your search parameters, filters them using criteria like keywords, budget, and client history, and drafts personalized proposals for you to respond to.</p>
 </div><img src="/images/workmate.png" className='h-[400px] rounded-full border-8 border-gray-400'/>
        </main> 
        <Footer />
